1eaeb60909a95a06cab648b5eb37e8d6d34fe4f8
Bilal El Idrissi <bel-idri@e1r9p7.1337.ma>
2022-11-10 17:47:15 +0100

    edit

--- get_next_line.c
+++ get_next_line.c
@@ -6,35 +6,50 @@
 /*   By: bel-idri <bel-idri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 15:21:22 by bel-idri          #+#    #+#             */
-/*   Updated: 2022/11/09 18:59:15 by bel-idri         ###   ########.fr       */
+/*   Updated: 2022/11/10 17:46:18 by bel-idri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-#include <fcntl.h>
-
 char *get_next_line(int fd)
 {
-	char	*buff;
-	ssize_t	nb;
+	char		*buff;
+	static char	*for_return;
+	ssize_t		nb;
+
+	for_return = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+
 	nb = 0;
-	buff = malloc(sizeof(char) * BUFFER_SIZE);
-	nb = read(fd, buff, BUFFER_SIZE);
-
-	printf ("%zd\n",nb);
-
+	buff = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	nb = read(fd, buff, BUFFER_SIZE); // '\0' is add auto or need to adding it
+	buff[BUFFER_SIZE] = '\0';
 	if (nb == -1)
-		return (NULL);
+		return (free(buff), NULL);
 
-	return (buff);
+	for_return = join_it(for_return, buff);
+
+	return (for_return);
 }
+
+
+
+
+
+
+
+#include <fcntl.h>
 
 int main()
 {
 	int fd = open("t",O_RDONLY);
-	char *s = get_next_line(fd);
-	printf("%s",s);
+	int i = 0;
+	while (i < 2)
+	{
+		char *s = get_next_line(fd);
+		printf("%s\n",s);
+		i++;
+	}
 }
 
 
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,36 +6,64 @@
 /*   By: bel-idri <bel-idri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 15:23:00 by bel-idri          #+#    #+#             */
-/*   Updated: 2022/11/09 18:37:21 by bel-idri         ###   ########.fr       */
+/*   Updated: 2022/11/10 17:46:39 by bel-idri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "get_next_line.h"
+#include "get_next_line.h"
 
-// void	my_ft_strcat(char *dst, const char *src)
-// {
-// 	size_t	k;
+size_t	ft_strlen(const char *s)
+{
+	size_t	x;
 
-// 	k = 0;
-// 	while (*dst)
-// 		dst++;
-// 	while (*src)
-// 		*dst = *(src)++;
-// 	*dst = '\0';
-// }
+	x = 0;
+	while (s[x])
+		x++;
+	return (x);
+}
 
-// int	newlin_end(char *buff,size_t nb)
-// {
-// 	size_t	i;
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*str;
+	size_t	i;
+	size_t	j;
 
-// 	i = 0;
-// 	if (nb != BUFFER_SIZE)
-// 		return (1);
-// 	while (buff[i])
-// 	{
-// 		if (buff[i] == '\n')
-// 			return (2);
-// 	}
-// }
+	str = (char *)malloc(100 * sizeof(char));
+	if (!str)
+		return (free(str), NULL);
+	i = -1;
+	j = -1;
+	while (++i < ft_strlen(s1))
+		str[i] = s1[i];
+	while (++j < ft_strlen(s2))
+	{
+		str[i] = s2[j];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
 
+int	is_newlin_end(char *buff,size_t nb)
+{
+	size_t	i;
 
+	i = 0;
+	if (nb != BUFFER_SIZE)
+		return (1);
+	while (buff[i])
+	{
+		if (buff[i] == '\n')
+			return (2);
+	}
+	return (0);
+}
+
+char	*join_it(char *for_return, char *buff)
+{
+
+	for_return = ft_strjoin(for_return, buff);
+
+	return(for_return);
+
+}
