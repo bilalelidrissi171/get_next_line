979c375d5b0fbdfe48a23d5d1444f48ce1f87d11
Bilal El Idrissi <bel-idri@student.1337.ma>
2022-11-16 22:55:53 +0100

    edit

---
+++ get_next_line_bonus.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bel-idri <bel-idri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/09 15:21:26 by bel-idri          #+#    #+#             */
+/*   Updated: 2022/11/16 22:30:15 by bel-idri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+char	*read_newline(int fd, char *my_backup);
+char	*extract_newline(char **my_backup);
+int		is_newline(char *s);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strdup(char *s1);
+size_t	ft_strlen(char *s);
+char	*extract_after_newline(char *my_backup);
+
+#endif
